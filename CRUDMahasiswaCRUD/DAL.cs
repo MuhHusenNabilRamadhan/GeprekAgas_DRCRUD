4e494345cd1534898a3a091bcacd3042424d428b
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-06-20 21:01:05 +0700

    menampilkan data chart

--- CRUDMahasiswaCRUD/DAL.cs
+++ CRUDMahasiswaCRUD/DAL.cs
@@ -214,6 +214,22 @@ namespace CRUDMahasiswaCRUD
 
             return dtMahasiswa;
         }
+        public DataTable getAllDataChart()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_DashBoard", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            da = new SqlDataAdapter(cmd);
+            dtMahasiswa = new DataTable();
+
+            da.Fill(dtMahasiswa);
+            return dtMahasiswa;
+        }
 
 
     }
