91eb4822eba0a781ae2782985558c2b8b6e9ecd3
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-06-20 21:00:34 +0700

    fungsi untuk menampilkan Data rekap dari prodi dan tanggal masuk

--- CRUDMahasiswaCRUD/DAL.cs
+++ CRUDMahasiswaCRUD/DAL.cs
@@ -194,6 +194,26 @@ namespace CRUDMahasiswaCRUD
             da.Fill(dtProdi);
             return dtProdi;
         }
+        public DataTable getDataRekap(string prodi, DateTime tanggalMasuk)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_Report", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@inProdi", prodi);
+            cmd.Parameters.AddWithValue("@inTglMsuk", tanggalMasuk.Year.ToString());
+
+            da = new SqlDataAdapter(cmd);
+
+            dtMahasiswa = new DataTable();
+            da.Fill(dtMahasiswa);
+
+            return dtMahasiswa;
+        }
 
 
     }
