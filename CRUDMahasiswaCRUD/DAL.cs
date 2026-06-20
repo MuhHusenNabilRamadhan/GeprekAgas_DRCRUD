7612e849ac03f517d51d5f66bf1e9662c423ffed
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-06-20 21:00:02 +0700

    fungsi menampilkan nama prodi dari tabel prodi

--- CRUDMahasiswaCRUD/DAL.cs
+++ CRUDMahasiswaCRUD/DAL.cs
@@ -178,6 +178,22 @@ namespace CRUDMahasiswaCRUD
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.ExecuteNonQuery();
         }
+        public DataTable getProdi()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("SELECT namaprodi from prodi", conn);
+            cmd.CommandType = CommandType.Text;
+
+            dtProdi = new DataTable();
+            da = new SqlDataAdapter(cmd);
+
+            da.Fill(dtProdi);
+            return dtProdi;
+        }
 
 
     }
