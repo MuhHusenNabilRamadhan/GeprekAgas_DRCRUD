8d1b3984d42612ce4e07842f577498c20aba5023
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2026-06-20 21:02:02 +0700

    fungsi untuk menampilkan Data chart melaui tahun

--- CRUDMahasiswaCRUD/DAL.cs
+++ CRUDMahasiswaCRUD/DAL.cs
@@ -230,7 +230,22 @@ namespace CRUDMahasiswaCRUD
             da.Fill(dtMahasiswa);
             return dtMahasiswa;
         }
+        public DataTable getDataChartByTahun(DateTime thMasuk)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand("sp_DashBoardByTahun", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
 
+            cmd.Parameters.AddWithValue("@inTglMsuk", thMasuk.Year);
+            da = new SqlDataAdapter(cmd);
+
+            dtMahasiswa = new DataTable();
+            da.Fill(dtMahasiswa);
+            return dtMahasiswa;
+        }
 
     }
 }
